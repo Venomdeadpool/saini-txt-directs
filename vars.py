2377b0204fad8b899a6c332121d13a7bae619d35
Venomdeadpool <satveer02062004@gmail.com>
2025-07-14 15:43:44 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -5,7 +5,7 @@ from os import environ
 
 API_ID = int(environ.get("API_ID", "20168440"))
 API_HASH = environ.get("API_HASH", "903ed715faf0c0729d9c5f221a6f2bc1")
-BOT_TOKEN = environ.get("BOT_TOKEN", "7794809702:AAGGU0JIg5NIXXoapyyT_albAuuj69PpoQk")
+BOT_TOKEN = environ.get("BOT_TOKEN", "7240735367:AAEhwiEGqM0-0f02TWGMQmWOm2svWgx2MWE")
 OWNER = int(environ.get("OWNER", "6027045449"))
 CREDIT = environ.get("CREDIT", "Venom") 
 TOTAL_USER = os.environ.get('TOTAL_USERS', '5680454765').split(',')
